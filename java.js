20f98733dd4b0ee6d270494e80fccb088b76b69b
Mhorvy <mhorvy@gmail.com>
2021-09-19 14:17:33 -0500

    tried getting timer to work

--- java.js
+++ java.js
@@ -3,6 +3,8 @@ const infoBox = document.querySelector(".info-box");
 const quitButton = document.querySelector('.buttons .quit');
 const restartButton = document.querySelector('.buttons .restart');
 const quizBox = document.querySelector('.quiz-box');
+const timeCount = document.querySelector(".timer .time-seconds");
+
 
 // if start quiz button clicked
 startButton.onclick = ()=>{
@@ -19,23 +21,25 @@ restartButton.onclick = ()=>{
     infoBox.classList.remove("activeInfo");
     quizBox.classList.add("activeQuiz");
     showQuestions(0);
+    startTimer(30);
 }
 
 let questionCount = 0;
 
-const nextButton = quizBox.querySelector(".next");
+const nextButton = document.querySelector("footer .next");
 
 //next button clicked
 nextButton.onclick = ()=>{
+    questionCount++;
     showQuestions(index);
-    }
+}
 
 
 // to get the questions array
 function showQuestions(index){
     const questionText = document.querySelector(".question-text");
     const optionList = document.querySelector(".answer-options");
-    let questionTag = '<span>'+ questionsList[index].numb + ". " + questionsList[index].question +'</span>';
+    let questionTag = '<span>'+ questionsList[index].numb + "." + questionsList[index].question +'</span>';
     let optionTag = '<div class="option"><span>'+ questionsList[index].optionsList[0] +'</span></div>'
     + '<div class="option"><span>'+ questionsList[index].optionsList[1] +'</span></div>'
     + '<div class="option"><span>'+ questionsList[index].optionsList[2] +'</span></div>'
@@ -51,10 +55,20 @@ function showQuestions(index){
 
 function optionSelected(answer){
     let selectedAnswer = answer.questionText;
-    let correctAnswer = questionsList[index].answer;
-    if(selectedAnswer === correctAnswer){
-    console.log ("Answer is correct");
+    let correctAnswer = questionsList[questionCount].answer;
+    if(selectedAnswer == correctAnswer){
+        answer.classList.add("correct")
+        console.log("Answer is correct");
     }else{
+        answer.classList.add("wrong")
         console.log("Answer is wrong");
     }
+}
+
+function startTimer(){
+    counter = setInterval(timer, 1000);
+    function timer(){
+        timeCount.textContent = time;
+        time--;
+}
 }
